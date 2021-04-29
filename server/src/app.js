b909c8e6ae2dfb948e60d4d8799ed40ae925de65
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-29 05:05:07 +0300

    new dependencies are added - first server

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,27 @@
 
-console.log("Helüü")
+// import
+const express = require('express');
+const cors = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Create app
+const app = express();
+
+// USE
+app.use(morgan('combine'))
+app.use(bodyParser.json)
+//app.use(cors())
+
+// GET
+app.get('/status', (req,res) => {
+    res.send({
+        message: "Hello World!"
+    })
+} )
+
+
+// Listen
+app.listen(process.env.PORT || 8081, () => ({ 
+    console.log("App is listening " + 8081)
+}))
